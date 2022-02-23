efbd1f2fbe5b52cc52f6c4886774b4fabec79598
jemima-stalin <100306579+jemima-stalin@users.noreply.github.com>
2022-02-23 18:40:00 +0000

    Second commit

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(() =>
+            {
+                Console.WriteLine("Task on thread {0} started.",
+                Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(3000);
+                Console.WriteLine("Task on thread {0} finished.",
+                Thread.CurrentThread.ManagedThreadId);
+            });
+
+            task.Start();
+            Console.WriteLine("This is the main thread.");
+
+            Task<int> task = new Task<int>(() =>
+            {
+                Thread.Sleep(3000);
+                return 2 + 3;
+            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
